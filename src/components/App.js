b6aa881a9a430f04be8d344dcae663c3982e53ab
Leandro Cunha <leandroscunha@gmail.com>
2018-09-28 17:25:58 -0300

    Add basic HTML structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+const App = () => (
+  <Fragment>
+    <header>
+      <h1>FindHotel - Best prices Guarantee</h1>
+    </header>
+    <div>Filters</div>
+    <div>Sort</div>
+    <div>Results</div>
+  </Fragment>
+);
+
+export default App;
