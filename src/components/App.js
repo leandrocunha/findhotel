eeddc0cb0a8cd4504d31da510fdd3c82d3aaffda
Leandro Cunha <leandroscunha@gmail.com>
2018-09-28 17:47:32 -0300

    Add basic CSS

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,8 @@ import React, { Fragment } from 'react';
 
 const App = () => (
   <Fragment>
-    <header>
-      <h1>FindHotel - Best prices Guarantee</h1>
+    <header className="Header">
+      <h1 className="Header__Brand">FindHotel - Best prices Guarantee</h1>
     </header>
     <div>Filters</div>
     <div>Sort</div>
